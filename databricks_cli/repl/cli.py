2a804b1e1e208dd4913fed18f69cc8b899b0f01a
Marek Brysa <marek.brysa@databricks.com>
2020-12-05 08:58:11 +0100

    wip

--- databricks_cli/repl/cli.py
+++ databricks_cli/repl/cli.py
@@ -1,59 +1,40 @@
 import click
-from click import ParamType
+from click.exceptions import Exit, ClickException
+from prompt_toolkit import PromptSession
 
 import databricks_cli
-from databricks_cli.click_types import PipelineSpecClickType, PipelineIdClickType, JsonClickType, OutputClickType
+from databricks_cli.configure.config import debug_option
+from databricks_cli.utils import CONTEXT_SETTINGS
 from databricks_cli.version import print_version_callback, version
-from databricks_cli.pipelines.api import PipelinesApi
-from databricks_cli.configure.config import provide_api_client, profile_option, debug_option
-from databricks_cli.utils import pipelines_exception_eater, CONTEXT_SETTINGS, pretty_format, \
-    error_and_quit
-from prompt_toolkit import PromptSession
-from tabulate import tabulate
 
-class ReplContext(object):
-    def __init__(self):
-        self.state = {}
-    def prompt(self):
-        """items = []
-        if 'object' in self.state:
-            items.append(self.state['object'])
-        if 'id' in self.state:
-            items.append(self.state['id'])
-        print(self.state)
-        print(items)
-        return ' '.join(items)"""
-        str(self.state)
 
-    def get_object(self):
-        return self.state.get('object')
-
-    def get_id(self):
-        return self.state.get('id')
-
-    def set_object(self, obj):
-        self.state = {
-            'object': obj
-        }
-
-    def set_id(self, obj_id):
-        self.state['id'] = obj_id
-
-@click.command(context_settings=CONTEXT_SETTINGS,
-             short_help='Utility to interact with the Databricks Delta Pipelines.')
+@click.command(context_settings=CONTEXT_SETTINGS, short_help='Interactive CLI REPL')
 @click.option('--version', '-v', is_flag=True, callback=print_version_callback,
               expose_value=False, is_eager=True, help=version)
+@click.option('--profile', required=False, default=None,
+              help='CLI connection profile to use. The default profile is "DEFAULT".')
 @debug_option
-@profile_option
-@provide_api_client
-def repl_group(api_client):  # pragma: no cover
+def repl_group(profile):  # pragma: no cover
     """
-    Utility to interact with the Databricks pipelines.
+    Interactive CLI REPL.
     """
     ctx = {}
+    if profile is not None:
+        ctx['profile'] = profile
+
+    ctx_options = ['job-id', 'profile', 'cluster-id', 'run-id']
+
     session = PromptSession()
 
+    click.echo(
+        'Welcome to the Databricks CLI REPL!\n'
+        'Set context by doing e.g. `--job-id 123`, unset with `--job-id`.\n'
+        'Use Databricks CLI commands as usual and parameters for {} will be '
+        'provided from the current context.'.format(", ".join(ctx_options)))
+    click.echo('Press Ctrl-D to exit.')
+
     while True:
+        click.echo()
         try:
             cmd = session.prompt('{} > '.format(str(ctx)))
         except KeyboardInterrupt:
@@ -61,28 +42,39 @@ def repl_group(api_client):  # pragma: no cover
         except EOFError:
             break
 
-        words = cmd.split(' ')
-        first = words[0]
+        cmdline = cmd.split()
+        if len(cmdline) == 0:
+            continue
+        first = cmdline[0]
         second = None
-        if len(words) > 1:
-            second = words[1]
+        if len(cmdline) > 1:
+            second = cmdline[1]
 
-        if first == 'jobs':
-            databricks_cli.jobs.cli.list_cli.callback(output='table')
-        elif first == 'job':
-            if second == 'get':
-                databricks_cli.jobs.cli.get_cli.callback(job_id=ctx.get('job_id'))
+        ctx_option = first[2:]
+        if ctx_option in ctx_options:
+            if second is None:
+                ctx.pop(ctx_option, None)
             else:
-                ctx['job_id'] = second
-        elif first == 'runs':
-            databricks_cli.runs.cli.list_cli()
-            databricks_cli.runs.cli.list_cli.callback(job_id=ctx.get('job_id'), output='table')
-        elif first == 'run':
-            if second == 'get':
-                databricks_cli.runs.cli.get_cli.callback(run_id=ctx.get('run_id'))
-            else:
-                ctx['run_id'] = second
+                ctx[ctx_option] = second
         else:
-            ctx.pop('run_id', None)
-
-
+            try:
+                click_ctx = databricks_cli.cli.cli.make_context("databricks-cli", cmdline)
+                cmd_name, group1, args = databricks_cli.cli.cli.resolve_command(click_ctx, cmdline)
+                if len(args) == 0 or group1 is None:
+                    click.echo("Command not found.", err=True)
+                    continue
+                cmd_name, cmd, args = group1.resolve_command(click_ctx, args)
+                if cmd is None:
+                    click.echo("Command not found.", err=True)
+                    continue
+                for param in cmd.params:
+                    param_name = param.name.replace('_', '-')
+                    if param_name in ctx:
+                        cmdline.append('--{}'.format(param_name))
+                        cmdline.append(ctx[param_name])
+                click_ctx = databricks_cli.cli.cli.make_context("databricks-cli", cmdline)
+                databricks_cli.cli.cli.invoke(click_ctx)
+            except Exit:
+                pass
+            except ClickException as e:
+                click.echo(e, err=True)
